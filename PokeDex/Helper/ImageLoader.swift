39442bc0dbff1e5e11ae71bd7c7d65aa32aa4bbd
berkangzgn <bgezgin@protonmail.com>
2022-04-25 20:43:16 +0300

    Helper created

---
+++ PokeDex/Helper/ImageLoader.swift
@@ -0,0 +1,56 @@
+//
+//  ImageLoader.swift
+//  PokeDex
+//
+//  Created by Berkan Gezgin on 25.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class ImageLoader: ObservableObject {
+    private var cache = CachedImage()
+    @Published var image: UIImage?
+
+    init(urlString: String) {
+        if getImageCache(url: urlString) {
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                guard let image = UIImage(data: data) else {
+                    return
+                }
+                
+                self.image = image
+                self.cache.setCache(url: urlString, image: image)
+            }
+        }
+        task.resume()
+    }
+    
+    func getImageCache(url: String) -> Bool {
+        guard let image = cache.getCache(url: url) else {
+            return false
+        }
+        self.image = image
+        return true
+    }
+}
+
+class CachedImage {
+    var cache = NSCache<NSString, UIImage>()
+    
+    func getCache(url: String) -> UIImage? {
+        return cache.object(forKey: NSString(string: url))
+    }
+    
+    func setCache(url: String, image: UIImage) {
+        cache.setObject(image, forKey: NSString(string: url))
+    }
+}
