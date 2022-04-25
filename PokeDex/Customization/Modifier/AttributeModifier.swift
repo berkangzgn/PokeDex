051853d29e61f1c08fab3069f183ed45ec6d2bd9
berkangzgn <bgezgin@protonmail.com>
2022-04-25 21:04:25 +0300

    Some pages removed

---
+++ PokeDex/Customization/Modifier/AttributeModifier.swift
@@ -0,0 +1,20 @@
+//
+//  AttributeModifier.swift
+//  PokeDex
+//
+//  Created by Berkan Gezgin on 25.04.2022.
+//
+
+import SwiftUI
+
+struct AttributeModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AttributeModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        AttributeModifier()
+    }
+}
