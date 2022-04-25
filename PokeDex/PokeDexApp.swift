99236b96ec24503183c1aca861f87200f212b51c
berkangzgn <bgezgin@protonmail.com>
2022-04-25 20:31:45 +0300

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Berkan Gezgin on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
